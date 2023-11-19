10214b27ac2285e6a03662dbc0bb94274809cda8
rtef119 <rtef119@gmail.com>
2023-11-19 12:30:37 +0100

    dz10

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ connection = sqlite3.connect("Dz10.sl3", 5)
 
 cur = connection.cursor()
 
+cur.execute("INSERT INTO Dz_10 (name, temperature) VALUES ('Kremenchyg','22');")
+
+connection.commit()
+import sqlite3
+
+connection = sqlite3.connect("Dz10.sl3", 5)
+
+cur = connection.cursor()
+
 cur.execute("CREATE TABLE Dz_10 (name TEXT, temperature TEXT);")
 
 connection.commit()
+connection.close()
 connection.close()
